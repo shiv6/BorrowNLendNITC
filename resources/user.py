08da8c6ece0e2a8d2c0d0344acd2e5ce70897573
Sumit Yadav <careeratedjing@gmail.com>
2020-03-12 10:53:42 +0545

    added token refresh resolved

--- resources/user.py
+++ resources/user.py
@@ -1,9 +1,9 @@
 import traceback
 import datetime
-from flask_restful import Resource, reqparse, jwt_refresh_token_required
+from flask_restful import Resource, reqparse
 from flask import request
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required, get_raw_jwt
+from flask_jwt_extended import create_access_token, jwt_refresh_token_required, create_refresh_token, get_jwt_identity, jwt_required, get_raw_jwt
 
 from schemas.user import UserSchema
 from models.user import UserModel
