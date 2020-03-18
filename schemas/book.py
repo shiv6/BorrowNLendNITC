cedad7604ed90a751823e6be4dff38c5be6ab5fe
Sumit Yadav <careeratedjing@gmail.com>
2020-03-18 22:05:23 +0545

    added foriegn key to book json dump

--- schemas/book.py
+++ schemas/book.py
@@ -1,5 +1,7 @@
 from ma import ma
+from marshmallow_sqlalchemy import fields
 from models.book import BookModel
+from schemas.user import UserSchema
 from db import db
 
 class BookSchema(ma.ModelSchema):
@@ -8,3 +10,4 @@ class BookSchema(ma.ModelSchema):
         sqla_session = db.session
         dump_only = ("id","is_borrowed")
         datetimeformat = "%d-%m-%Y"
+    owner = fields.Nested(UserSchema)
